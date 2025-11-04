63ea9f8c048850893c9bea9c2582033516c1a5ca
Lukasz Glowacki <lukasz.glowacki@sourcefactory.pl>
2025-11-04 22:46:44 +0100

    Keycloak user management from frontend + fixes

---
+++ virtual-school-backend/src/main/java/com/virtualschool/virtual_school_backend/controller/KeycloakController.java
@@ -0,0 +1,33 @@
+package com.virtualschool.virtual_school_backend.controller;
+
+import com.virtualschool.virtual_school_backend.dto.UserDTO;
+import com.virtualschool.virtual_school_backend.service.KeycloakService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class KeycloakController {
+
+    private final KeycloakService keycloakService;
+
+    public KeycloakController(KeycloakService keycloakService) {
+        this.keycloakService = keycloakService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UserDTO userDTO) {
+        keycloakService.createUser(
+                userDTO.getUsername(),
+                userDTO.getPassword(),
+                userDTO.getFirstName(),
+                userDTO.getLastName(),
+                userDTO.getEmail(),
+                userDTO.getRole()
+        );
+        return ResponseEntity.ok().build();
+    }
+}
